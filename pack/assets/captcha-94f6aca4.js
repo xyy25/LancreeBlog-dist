70eca03deb4e575acf298cad9e74dffcd917af50
xyy25 <841745697@qq.com>
2023-10-07 18:02:14 +0800

    调试图片上传功能

---
+++ pack/assets/captcha-94f6aca4.js
@@ -0,0 +1,13 @@
+import { A as Api } from "./app-6f3b7185.js";
+Api.prototype.getCaptcha = function() {
+  return this.req.get("/captcha");
+};
+Api.prototype.validateCaptcha = function(key, dots, remove = false) {
+  return this.req.post("/captcha", {
+    captcha: {
+      key,
+      dots,
+      remove
+    }
+  });
+};
