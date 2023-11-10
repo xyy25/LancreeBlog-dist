665f9de3cc7c41d4b004adc60a55b439fa4207eb
xyy25 <841745697@qq.com>
2023-11-10 15:08:16 +0800

    发布文章

---
+++ pack/assets/captcha-d45a1192.js
@@ -0,0 +1,1 @@
+import{A as t}from"./app-cd170f7f.js";import"vue";import"katex";import"@vicons/carbon";t.prototype.getCaptcha=function(){return this.req.get("/captcha")};t.prototype.validateCaptcha=function(p,a,r=!1){return this.req.post("/captcha",{captcha:{key:p,dots:a,remove:r}})};
