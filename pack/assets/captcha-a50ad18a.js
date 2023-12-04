a0f89df21f98bec6eaeaad2e8f07f68e7eb4d42b
xyy25 <841745697@qq.com>
2023-12-04 16:39:47 +0800

    更新文章

---
+++ pack/assets/captcha-a50ad18a.js
@@ -0,0 +1,1 @@
+import{A as t}from"./app-f29f3090.js";import"vue";import"katex";t.prototype.getCaptcha=function(){return this.req.get("/captcha")};t.prototype.validateCaptcha=function(a,p,r=!1){return this.req.post("/captcha",{captcha:{key:a,dots:p,remove:r}})};
