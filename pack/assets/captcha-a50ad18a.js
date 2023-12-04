50d73f1b447cffc3be63f8f79d192d44759802fd
xyy25 <841745697@qq.com>
2023-12-04 17:00:58 +0800

    修复music

--- pack/assets/captcha-a50ad18a.js
+++
@@ -1,1 +0,0 @@
-import{A as t}from"./app-f29f3090.js";import"vue";import"katex";t.prototype.getCaptcha=function(){return this.req.get("/captcha")};t.prototype.validateCaptcha=function(a,p,r=!1){return this.req.post("/captcha",{captcha:{key:a,dots:p,remove:r}})};
