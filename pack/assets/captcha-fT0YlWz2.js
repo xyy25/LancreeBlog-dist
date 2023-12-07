7cec3b2b72ffe8a5181d9585591ad45f6187ff51
xyy25 <841745697@qq.com>
2023-12-07 14:39:17 +0800

    修复cdn，优化music页面

--- pack/assets/captcha-fT0YlWz2.js
+++
@@ -1,1 +0,0 @@
-import{A as t}from"./app-PNu-wVX2.js";import"vue";import"katex";t.prototype.getCaptcha=function(){return this.req.get("/captcha")};t.prototype.validateCaptcha=function(a,p,r=!1){return this.req.post("/captcha",{captcha:{key:a,dots:p,remove:r}})};
